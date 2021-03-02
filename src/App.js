ae7e45a7f2c7854e17bc19528879870cc524856e
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2021-03-03 00:10:00 +0700

    Install react, react-dom, and create main files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Create a React Development Build from Scratch</h1>
+        </div>
+    );
+};
+
+export default App;
