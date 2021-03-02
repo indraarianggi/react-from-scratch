8601f6c908ea30f8a02f7a1445f478f46fe004c6
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2021-03-03 00:41:03 +0700

    Install and configure Webpack, Babel and ESLint

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "budle.js",
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+                options: {
+                    emitWarning: true,
+                    failOnError: false,
+                    failOnWarning: false,
+                },
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html",
+        }),
+    ],
+};
